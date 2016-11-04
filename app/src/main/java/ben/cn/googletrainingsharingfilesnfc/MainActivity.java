ad52d3bf1c05cff32b1c09eceabc231b20fa51c3
p_ybinyang(杨应彬) <p_ybinyang@tencent.com>
2016-11-04 11:43:54 +0800

    Test for Android Beam File Transfer Support

--- app/src/main/java/ben/cn/googletrainingsharingfilesnfc/MainActivity.java
+++ app/src/main/java/ben/cn/googletrainingsharingfilesnfc/MainActivity.java
@@ -1,13 +1,44 @@
 package ben.cn.googletrainingsharingfilesnfc;
 
+import android.content.pm.PackageManager;
+import android.nfc.NfcAdapter;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    NfcAdapter mNfcAdapter;
+    // Flag to indicate that Android Beam is available
+    boolean mAndroidBeamAvailable  = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PackageManager packageManager = getPackageManager();
+        // NFC isn't available on the device
+        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_NFC)) {
+            /*
+             * Disable NFC features here.
+             * For example, disable menu items or buttons that activate
+             * NFC-related features
+             */
+            Toast.makeText(this, "NFC not supported.", Toast.LENGTH_SHORT).show();
+            // Android Beam file transfer isn't supported
+        } else if (Build.VERSION.SDK_INT <
+                Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            // If Android Beam isn't available, don't continue.
+            mAndroidBeamAvailable = false;
+            /*
+             * Disable Android Beam file transfer features here.
+             */
+            Toast.makeText(this, "Android Beam file transfer not supported.", Toast.LENGTH_SHORT).show();
+            // Android Beam file transfer is available, continue
+        } else {
+            mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        }
     }
 }
